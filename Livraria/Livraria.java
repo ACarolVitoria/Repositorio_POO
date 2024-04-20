0bd7e5ef826e6c6cb5dca259f1dca1aa5fac1691
ACarolVitoria <162060018+ACarolVitoria@users.noreply.github.com>
2024-04-20 09:30:33 -0300

    Update Livraria.java

--- Livraria/Livraria.java
+++ Livraria/Livraria.java
@@ -4,21 +4,34 @@ import java.util.ArrayList;
 
 public class Livraria {
     
-    public ArrayList<Livro> Livros;
+    public ArrayList<Livro> livros;
 
     public Livraria(){
-        this.Livros = new ArrayList<>();
+        this.livros = new ArrayList<>();
     }
 
     public void cadastrarLivro(Livro livro){
-        this.Livros.add(livro);
+        this.livros.add(livro);
     }
 
     public String listarLivros(){
         String listaLivros = "";
-        for (Livro l : this.Livros){
-            listaLivros += l.id + "  -  " + l.titulo + "\n";
+        for (Livro l : this.livros) {
+            listaLivros += l.id + " - " + l.titulo + "\n";
         }
         return listaLivros;
     }
+
+    public String detalharLivroPorId(int id){
+        String detalheLivro = "Livro não encontrado.\n";
+
+        for (Livro livro : livros) {
+            if (livro.id == id){
+                detalheLivro = livro.toString();
+            }
+        }
+
+        return detalheLivro;
+    }
+
 }
