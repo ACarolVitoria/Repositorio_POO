4e0d47f9279238539907ed21cb65d9a78951d964
ACarolVitoria <162060018+ACarolVitoria@users.noreply.github.com>
2024-05-11 10:59:28 -0300

    Create Calculadora.java

---
+++ Polimorfismo/Calculadora.java
@@ -0,0 +1,14 @@
+package Polimorfismo;
+
+public class Calculadora {
+    
+    public int somar(int a, int b){
+        System.err.println("Somando int: ");
+        return a + b;
+    }
+
+    public double somar(double a, double b){
+        System.err.println("Somando int: ");
+        return a + b;
+    }
+}
